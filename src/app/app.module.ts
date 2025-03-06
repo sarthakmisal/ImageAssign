f84955ca91b193a0a876dd1abe4978fbe59d2682
Sarthak Misal <sarthakrmisal05@gmail.com>
2025-03-07 00:49:09 +0530

    Firebase NOtifications added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { FirebaseX } from '@awesome-cordova-plugins/firebase-x/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [FirebaseX,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FirebaseX } from '@awesome-cordova-plugins/firebase-x/ngx';
+import { Platform } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,7 +10,33 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
+  constructor(private platform: Platform, private firebase: FirebaseX) {
+    this.initializeApp();
+  }
 
-  constructor() {}
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.setupFirebase();
+    });
+  }
 
+  setupFirebase() {
+    // Get the FCM token
+    this.firebase.getToken().then(token => {
+      console.log('FCM Token:', token);
+    }).catch(error => {
+      console.error('Error getting token:', error);
+    });
+
+    // Listen for incoming notifications
+    this.firebase.onMessageReceived().subscribe(data => {
+      console.log('Message received:', data);
+      alert(JSON.stringify(data)); // Handle the notification
+    });
+
+    // Listen for token refresh
+    this.firebase.onTokenRefresh().subscribe(token => {
+      console.log('New FCM Token:', token);
+    });
+  }
 }
